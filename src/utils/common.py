b81e3e2f38244fe1c3f127286b68f6216abb91d6
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-06-18 15:17:03 +0300

    feat: remove special symbols from itos

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,5 +1,16 @@
-def stoi_many(seqs, vocab):
-    sents = [[vocab.itos[i] for i in s] for s in seqs]
+"""
+This module contains common helper functions
+"""
+
+SPECIAL_TOKENS = {'<bos>', '<eos>', '<pad>'}
+
+
+def itos_many(seqs, vocab):
+    """
+    Converts sequences of token ids to normal strings
+    """
+    sents = [[vocab.itos[i] for i in seq] for seq in seqs]
+    sents = [[t for t in seq if not t in SPECIAL_TOKENS] for seq in seqs]
     sents = [' '.join(s).replace('@@ ', '') for s in sents]
 
     return sents
