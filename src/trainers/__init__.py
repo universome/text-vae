2fd5d44dc7fc308fa468b71e7a59e18f45b4b974
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-07-04 17:08:44 +0300

    feat: try simple noisy AE

--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -1,7 +1,9 @@
 from .vae_trainer import VAETrainer
 from .ae_trainer import AETrainer
+from .noisy_ae_trainer import NoisyAETrainer
 
 __all__ = [
     "VAETrainer",
-    "AETrainer"
+    "AETrainer",
+    "NoisyAETrainer"
 ]
